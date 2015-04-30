4f4f8637a279d728ddcf2a61e146de7bfcbb8fd0
Nicolin Chen <nicoleotsuka@gmail.com>
2015-04-29 20:06:34 -0700

    CMPE-244: Project: integrate motion task
    Signed-off-by: Nicolin Chen <nicoleotsuka@gmail.com>

--- L5_Application/source/power_wifi.cpp
+++ L5_Application/source/power_wifi.cpp
@@ -14,6 +14,7 @@
 #define WIFI_CMD_GIVE_STATUS     2
 #define WIFI_CMD_CTL_DIR         3
 #define WIFI_CMD_TERMINATE       4
+#define WIFI_CMD_MOVE            5
 #define WIFI_CMD_IDLE            255
 
 #define WIFI_DATA_MAX            256
@@ -32,8 +33,27 @@
 #define WIFI_STATUS_IDX_MPOS    4
 #define ADC_PORT                2
 
+/**
+ * Motion Control Package Structure
+ *
+ * || 1 byte  |   1 byte    |   1 byte    |   1 byte    ||
+ * || Command | Parameter 1 | Parameter 2 | Parameter 3 ||
+ */
+#define WIFI_MOVE_IDX_CMD       0
+#define WIFI_MOVE_IDX_PARAM1    1
+#define WIFI_MOVE_IDX_PARAM2    2
+#define WIFI_MOVE_IDX_PARAM3    3
+
+#define CMD_UNPACK(p)           (((p) >> 24) & 0xff)
+#define PARAM1_UNPACK(p)        (((p) >> 16) & 0xff)
+#define PARAM2_UNPACK(p)        (((p) >> 8) & 0xff)
+#define PARAM3_UNPACK(p)        (((p) >> 0) & 0xff)
+
 static char position = 0;
+static unsigned char busy = 0;
 static unsigned char error = 0;
+static QueueHandle_t comm_queue;
+static QueueHandle_t motion_queue;
 static SemaphoreHandle_t signalSlaveHeartbeat;
 
 static void wifi_slave_heartbeat()
@@ -57,8 +77,8 @@ static int wifi_pkt_decoding(mesh_packet_t *pkt)
     char len = pkt->info.data_len;
     char cmd = pkt->data[0];
     char pkg[WIFI_DATA_MAX];
+    uint32_t tmp;
     int i = 0;
-
 
     if (cmd != WIFI_CMD_IDLE) {
         if (mesh_get_node_address() == WIFI_MASTER_ADDR)
@@ -105,6 +125,12 @@ static int wifi_pkt_decoding(mesh_packet_t *pkt)
             if (mesh_get_node_address() == WIFI_MASTER_ADDR)
                 break;
             break;
+        case WIFI_CMD_MOVE:
+            tmp = pkt->data[WIFI_MOVE_IDX_CMD] << 24 |
+                  pkt->data[WIFI_MOVE_IDX_PARAM1] << 16;
+            if (!xQueueSend(comm_queue, &tmp, 1000))
+                printf("failed to pass MOVE cmd to next layer\n");
+            break;
         case WIFI_CMD_IDLE:
             break;
         default:
@@ -139,6 +165,102 @@ static void wifi_slave_heartbeat_task(void *p)
     while(1);
 }
 
+static void mid_comm_task(void *p)
+{
+    uint32_t word;
+    uint8_t steps;
+
+    printf("-----Task %s started\n", __func__);
+    while (1) {
+        if (!xQueueReceive(comm_queue, &word, 1000))
+            continue;
+
+        steps = PARAM1_UNPACK(word);
+        switch (CMD_UNPACK(word)){
+            case WIFI_CMD_MOVE:
+                printf("sending steps %d\n", steps);
+                if (busy) {
+                    //TO DO FUNCTION ALREADY BUSY;
+                    break;
+                }
+                if (!xQueueSend(motion_queue, &steps, 1000))
+                    printf("The command could not be sent to motion queue\n");
+                break;
+            case WIFI_CMD_GET_STATUS:
+                /* TODO Need to figure out a way to give feedback to wifi layer */
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+#define P2_0 (1 << 0)
+#define P2_1 (1 << 1)
+#define P2_2 (1 << 2)
+
+#define DIRECTION_PIN P2_1
+#define ENABLE_PIN    P2_0
+#define STEP_PIN      P2_2
+
+#define SPEED_MS  5
+
+#define DRIVE_ON true
+#define DRIVE_OFF false
+
+#define CW true
+#define CCW false
+
+void enableDrive(bool state)
+{
+    // P2.0 for ENABLE
+    if (state)
+        LPC_GPIO2->FIOSET = ENABLE_PIN;
+    else
+        LPC_GPIO2->FIOCLR = ENABLE_PIN;
+}
+
+void toggleStep(void)
+{
+    if ((bool) (LPC_GPIO2->FIOPIN & STEP_PIN))
+        LPC_GPIO2->FIOCLR = STEP_PIN;
+    else
+        LPC_GPIO2->FIOSET = STEP_PIN;
+}
+
+void setDirection(bool direction)
+{
+    if (direction)
+        LPC_GPIO2->FIOSET = DIRECTION_PIN;
+    else
+        LPC_GPIO2->FIOCLR = DIRECTION_PIN;
+}
+
+void setStep(bool state)
+{
+    if (state)
+        LPC_GPIO2->FIOSET = STEP_PIN;
+    else
+        LPC_GPIO2->FIOCLR = STEP_PIN;
+}
+
+static void motion_task(void *p)
+{
+    uint8_t steps;
+    printf("-----Task %s started\n", __func__);
+    while (1) {
+        if (!xQueueReceive(motion_queue, &steps, 1000))
+            continue;
+        printf("%s: move %d steps\n", __func__, steps);
+        enableDrive(false);
+        setDirection(CCW);
+        while (steps--) {
+            toggleStep();
+            vTaskDelay(SPEED_MS);
+        }
+    }
+}
+
 void power_wifi_init()
 {
     char cmd = WIFI_CMD_REQPWR;
@@ -147,10 +269,21 @@ void power_wifi_init()
     LPC_PINCON->PINSEL1 &= ~(0x3 << 20);
     LPC_PINCON->PINSEL1 |= 0x1 << 20;
 
+    // set up enable,m direction, and step GPIO
+    LPC_GPIO2->FIODIR |= DIRECTION_PIN + ENABLE_PIN + STEP_PIN;
+    // set up pull down for all
+    LPC_PINCON->PINMODE4 |= 3 + (3 << 2) + (3 << 4);
+    LPC_PINCON->PINMODE_OD2 = DIRECTION_PIN + ENABLE_PIN + STEP_PIN;
+
     signalSlaveHeartbeat = xSemaphoreCreateBinary();
 
-    xTaskCreate(wifi_receive_task, "wifi_receive", STACK_BYTES(2048), 0, PRIORITY_LOW, NULL);
-    xTaskCreate(wifi_slave_heartbeat_task, "wifi_slave_heartbeat", STACK_BYTES(2048), 0, PRIORITY_HIGH, NULL);
+    comm_queue = xQueueCreate(10, sizeof(uint32_t));
+    motion_queue = xQueueCreate(10, sizeof(uint8_t));
+
+    xTaskCreate(wifi_receive_task, "wifi_receive", STACK_BYTES(2048), 0, PRIORITY_MEDIUM, NULL);
+    xTaskCreate(wifi_slave_heartbeat_task, "wifi_slave_heartbeat", STACK_BYTES(2048), 0, PRIORITY_MEDIUM, NULL);
+    xTaskCreate(mid_comm_task, "mid_comm_task", STACK_BYTES(2048), 0, PRIORITY_MEDIUM, NULL);
+    xTaskCreate(motion_task, "motion_task", STACK_BYTES(2048), 0, PRIORITY_MEDIUM, NULL);
 
     if (!wireless_send(WIFI_MASTER_ADDR, mesh_pkt_ack, &cmd, sizeof(cmd), 0))
         printf("Slave: Failed to send REQPWR\n");
